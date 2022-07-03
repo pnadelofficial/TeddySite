c2cadaef352cb15e3e6bca40c16ac82a20cc63c2
Ted Burns <tedburns745@gmail.com>
2022-07-03 20:26:55 +0000

    allowed host update

--- mainsite/mainsite/settings.py
+++ mainsite/mainsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(t+ppc@$n60o!oj+no%0py*7k0frllry-e6s%=yhe#!oe9^1jk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tedburns.pythonanywhere.com']
 
 
 # Application definition
