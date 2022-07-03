6919e72c55439614ea32d11e1da14b51c823a8e2
pnadelofficial <peter.m.nadel@gmail.com>
2022-07-03 21:54:46 +0000

    static config

--- mainsite/mainsite/urls.py
+++ mainsite/mainsite/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from projects.views import home_view, projects_index
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name='home'),
     path('projects/', include("projects.urls"))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
