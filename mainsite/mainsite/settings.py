a63c865e2751c35720e2a72e9535a65e8f235334
pnadelofficial <peter.m.nadel@gmail.com>
2022-07-04 17:08:25 +0000

    media update

--- mainsite/mainsite/settings.py
+++ mainsite/mainsite/settings.py
@@ -122,6 +122,9 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\', '/')
+MEDIA_URL = '/media/'
+
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'static'),
 # )
