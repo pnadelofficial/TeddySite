1a6765fff698f2e5a3f5a738fd1f17ae775fe146
pnadelofficial <peter.m.nadel@gmail.com>
2022-07-04 21:00:12 +0000

    urls update

--- mainsite/projects/urls.py
+++ mainsite/projects/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("",views.projects_index,name='projects_index'),
     path("writing/",views.writing_index,name='writing_index'),
     path("music/",views.music_index,name='music_index'),
-    path("music/<str:album>", views.album_detail, name='album_detail')
+    path("music/<int:album>", views.album_detail, name='album_detail')
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
