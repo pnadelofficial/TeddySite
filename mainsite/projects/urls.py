96e67fa5f7c8a73c895c1f687829efd518c53e5f
pnadelofficial <peter.m.nadel@gmail.com>
2022-07-05 23:38:35 +0000

    writing_index links

--- mainsite/projects/urls.py
+++ mainsite/projects/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path("",views.projects_index,name='projects_index'),
     path("writing/",views.writing_index,name='writing_index'),
     path("music/",views.music_index,name='music_index'),
-    path("music/<int:album>", views.album_detail, name='album_detail')
+    path("music/<int:album>", views.album_detail, name='album_detail'),
+    path("writing/<int:collection>",views.collection_detail, name='collection_detail')
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
